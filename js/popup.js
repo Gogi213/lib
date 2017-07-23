70b2166bc26cfc99bae687a138f6a1d48b243ef4
Gogi213 <1993georgiy@gmail.com>
2017-07-23 15:08:47 +0300

    final cut

--- js/popup.js
+++ js/popup.js
@@ -12,3 +12,29 @@ function popup() {
   headBtn.classList.toggle("none");
   backtosite.classList.toggle("flex");
 }
+
+// window.onload = function() {
+//   var search = document.getElementsById("search");
+//   search.onmouseover = visible;
+// }
+//
+// function visible(event) {
+//   var class = event.target;
+//   class.classList.toggle("visibility");
+// }
+
+
+window.onload = function () {
+  //ищем элемент по селектору
+  var a = document.querySelector('#search');
+  //вешаем на него события
+  a.onmouseout = function(e) {
+    document.getElementById('search__field').style.visibility='hidden';
+    document.getElementById('search__magnifier').style.visibility='visible';
+  }
+
+  a.onmouseover = function(e) {
+    document.getElementById('search__field').style.visibility='visible';
+    document.getElementById('search__magnifier').style.visibility='hidden';
+  };
+}
